b19ed08261a0f6a339baa31d7b0110f021dd0a85
SUNGMYEONGGI <smgsmg226@gmail.com>
2024-04-12 15:17:59 +0900

    Update: Map 마커표시 수정

--- map.py
+++ map.py
@@ -2,10 +2,11 @@ import streamlit as st
 import pandas as pd
 import json
 import requests
+import pydeck as pdk
 
 # Geocoding API를 통해 주소를 위도, 경도로 변환하는 함수
 def geocode(address):
-    api_key = 'API_KEYS'  # Google Maps API 키
+    api_key = 'API_KEY'  # Google Maps API 키
     base_url = f'https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={api_key}'
     response = requests.get(base_url)
     data = response.json()
@@ -22,7 +23,7 @@ def load_jobs_from_json(json_filename):
     return jobs
 
 # JSON 파일 이름
-json_filename = 'Crawling/job_data.json'
+json_filename = 'Crawling/job_data2.json'
 
 # JSON 파일에서 채용 정보 데이터를 읽어오기
 jobs = load_jobs_from_json(json_filename)
@@ -32,13 +33,44 @@ map_data_list = []
 
 # 각 채용 정보의 지역을 위도, 경도로 변환
 for job in jobs:
-    lat, lon = geocode(job['region'])
+    lat, lon = geocode(job['address'])
     if lat is not None and lon is not None:
-        map_data_list.append({'lat': lat, 'lon': lon, 'title': job['title'], 'company': job['company']})
+        map_data_list.append({
+            'lat': lat,
+            'lon': lon,
+            'title': job['title'],
+            'company': job['company'],
+            'tooltip': job['company']
+        })
 
 # 데이터프레임 생성
 map_data = pd.DataFrame(map_data_list)
 
-# 스트림릿을 사용하여 지도를 표시
-st.subheader('Map of Data')
-st.map(map_data)
+# pydeck 레이어 설정
+layer = pdk.Layer(
+    'ScatterplotLayer',
+    data=map_data,
+    get_position=['lon', 'lat'],
+    get_color=[255, 0, 0],  # 마커 색상은 빨간색으로 설정
+    get_radius=300,  # 마커의 반경
+    pickable=True,
+    tooltip=True
+)
+
+# pydeck 뷰포트 설정
+view_state = pdk.ViewState(
+    latitude=map_data['lat'].mean(),
+    longitude=map_data['lon'].mean(),
+    zoom=10
+)
+
+# pydeck 맵 생성
+r = pdk.Deck(
+    map_style='mapbox://styles/mapbox/light-v10',
+    initial_view_state=view_state,
+    layers=[layer],
+    tooltip={"text": "{tooltip}"}
+)
+
+# 스트림릿을 통해 pydeck 맵 표시
+st.pydeck_chart(r)
